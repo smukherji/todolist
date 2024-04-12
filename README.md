7a827bbf7dfaf514e8b3ff64414ebc971f5bad47
smukherji <mukherjisayan@gmail.com>
2024-04-12 07:43:35 -0500

    Added getByID, more details to readme

--- README.md
+++ README.md
@@ -1,9 +1,23 @@
-# todolist
+# TODOLIST
 TODO List with Azure functions and Cosmos DB
+
+## Setting up the environment
 
 Install the following dependencies:
 
 ```
 npm install mongoose
 npm install mongodb
+```
+
+## Testing
+
+Testing with curl:
+
+```
+curl -X POST --location http://localhost:7071/api/todo --header "Content-Type:application/json" --data '{"name":"shivali","items":[{"task":"testing","status":"todo"}]}' --verbose
+
+curl --location http://localhost:7071/api/todo/6618d335168fd1a2295031c --verbose
+
+ curl --location http://localhost:7071/api/todo --verbose
 ```
--- src/functions/todos.js
+++ src/functions/todos.js
@@ -1,6 +1,8 @@
 const { app } = require('@azure/functions');
-const mongoose = require ('mongoose')
-const { TodoModel } = require("./todoschema")
+const mongoose = require ('mongoose');
+const { ObjectId } = require('mongodb');
+const { TodoModel } = require("./todoschema");
+
 mongoose.connect(process.env.MONGO_DB_URI, { useNewUrlParser: true, useUnifiedTopology: true, autoIndex: true });
 const con = mongoose.connection;
 
@@ -12,6 +14,7 @@ try {
     console.log("Error: " + error);
 }
 
+// curl --location http://localhost:7071/api/todo --verbose
 app.http('getTodoList', {
     methods: ['GET'],
     authLevel: 'anonymous',
@@ -27,6 +30,33 @@ app.http('getTodoList', {
     }
 });
 
+// curl --location http://localhost:7071/api/todo/6618d335168fd1a2295031c --verbose
+app.http('getTodo', {
+    methods: ['GET'],
+    authLevel: 'anonymous',
+    route: 'todo/{id}',
+    handler: async (request, context) => {
+        context.log(`Http function getTodo processed request for url "${request.url}"`);
+        console.log('request.params.id', request.params.id);
+
+        const id = request.params.id;
+        if (ObjectId.isValid(id)) {
+            const todo = await con.model('TodoList').findOne({ _id: id });
+
+            if (todo) {
+                return {
+                    jsonBody: {todo: todo}
+                }
+            }
+        }
+        return {
+            status:404,
+            jsonBody: {error: "no todo found by that Id: " + request.params.id}
+        }
+    },
+});
+
+// curl -X POST --location http://localhost:7071/api/todo --header "Content-Type:application/json" --data '{"name":"shivali","items":[{"task":"testing","status":"todo"}]}' --verbose
 app.http('newTodo', {
     methods: ['POST'],
     authLevel: 'anonymous',
